1e7a217d67d505df07196f2fe48409347538517a
Septiandi <septiandi2209@gmail.com>
2025-07-19 23:24:46 +0700

    Update koneksi.php

--- config/koneksi.php
+++ config/koneksi.php
@@ -1,11 +1,9 @@
 <?php
-$host = 'electrotechbatam-septiandi2209.c.aivencloud.com';
-$port = 17293;
-$user = 'avnadmin';
-$pass = 'AVNS_64n3yPW_Ar4_otkz6FU';
-$db   = 'toko';
-
-$koneksi = mysqli_connect($host, $user, $pass, $db, $port);
+$host = "localhost";
+$user = "root";
+$pass = "";
+$db = "toko";
+$koneksi = mysqli_connect($host, $user, $pass, $db);
 if (!$koneksi) {
     die("Koneksi gagal: " . mysqli_connect_error());
 }
